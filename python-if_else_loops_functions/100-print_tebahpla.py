89fd9354a158237d3a069f2f0587ecdb5d081a62
agalotaibi <hopes.gmo@gmail.com>
2025-11-25 18:20:05 +0000

    solve task 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i - (32 if i % 2 != 0 else 0))), end="") 
+    print("{}".format(chr(i - (32 if i % 2 != 0 else 0))), end="")
