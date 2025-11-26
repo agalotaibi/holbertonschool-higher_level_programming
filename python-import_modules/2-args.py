1752a24c757cdf60c039baa60e22ef9f523df939
agalotaibi <hopes.gmo@gmail.com>
2025-11-26 10:49:28 +0000

    solve task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv[1:]
+count = len(argv)
+if __name__ == "__main__":
+    if count == 0:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
+    else:
+        print("{} arguments:".format(count))
+
+    if count > 0:
+            for i, arg in enumerate(argv):
+                position = i + 1
+                print("{}: {}".format(position, arg))
