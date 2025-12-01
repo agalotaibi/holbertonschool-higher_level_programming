d88ef274a29e51eaaf8e4b5d130c61953690b235
agalotaibi <hopes.gmo@gmail.com>
2025-12-01 08:55:04 +0000

    solve task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,0)
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,0)
+    new_first = tuple_a[0] + tuple_b[0]
+    new_second = tuple_a[1] + tuple_b[1]
+    new_tuple = (new_first, new_second)
+    return new_tuple
