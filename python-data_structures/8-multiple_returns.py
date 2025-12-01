2b8162610a102751bbbd0eee4db9aa25832cb947
agalotaibi <hopes.gmo@gmail.com>
2025-12-01 09:10:55 +0000

    solve task 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        result = (length, None)
+        return result
+    
+    first = sentence[0]
+    result = (length, first)
+    return result
