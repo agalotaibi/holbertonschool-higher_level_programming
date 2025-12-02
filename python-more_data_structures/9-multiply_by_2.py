55780ccf4f1391533fac249182f7061cfcd101fa
agalotaibi <hopes.gmo@gmail.com>
2025-12-02 10:45:45 +0000

    solve task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    update_dir = a_dictionary.copy()
     product = 2
-    for itm in a_dictionary.values():
-         product *= itm
-    return a_dictionary
+    for key in update_dir:
+         update_dir[key] = update_dir[key] * 2
+    return update_dir
