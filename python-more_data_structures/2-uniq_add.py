aeb40834b7b81bf72ddd73b15bb834de17ec91ef
agalotaibi <hopes.gmo@gmail.com>
2025-12-02 08:15:20 +0000

    solve task 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    total = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
+    for i in new_list:
+        total += i
+    
+    return total
