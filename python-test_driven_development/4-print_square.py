a35f9cc337aee1116f848fceed979f8bfea48e36
agalotaibi <hopes.gmo@gmail.com>
2025-12-04 16:07:17 +0000

    correct the code file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,7 +15,7 @@ def print_square(size):
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
     """
-    if not isinstance(size, int):
+    if isinstance(size, bool) or not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
