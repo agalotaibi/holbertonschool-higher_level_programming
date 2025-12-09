34625193e579a5ce02a04303fd2053682f3a1c4b
agalotaibi <hopes.gmo@gmail.com>
2025-12-09 15:50:30 +0000

    solve task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+"""
+class Rectangle:
+    """
+    This class defines a rectangle.
+    Currently, it is an empty class that does nothing.
+    """
+    pass
