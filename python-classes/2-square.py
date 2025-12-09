4cb0949aff70e3a97819b5b07139235b2bc048e3
agalotaibi <hopes.gmo@gmail.com>
2025-12-09 07:49:46 +0000

    solve task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
 This module defines a class Square based on 1-square.py.
-It adds a private instance attribute 'size'.
+It adds a private instance attribute 'size' with validation.
 """
+
 
 class Square:
     """
@@ -12,18 +13,18 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new Square instance.
-        
+
         Args:
-        size (int): The size of the square. Defaults to 0.
-        
+            size (int): The size of the square. Defaults to 0.
+
         Raises:
-            TypeError: If size is not integers.
+            TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
