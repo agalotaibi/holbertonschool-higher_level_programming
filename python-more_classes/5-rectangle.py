4f4331c8ed678c7443942669baa195c730531edd
agalotaibi <hopes.gmo@gmail.com>
2025-12-12 12:22:17 +0000

    solve task 5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -95,6 +95,6 @@ class Rectangle:
 
     def __del__(self):
         """
-        Destructor that prints a message when the Rectangle instance is deleted.
+        Prints a message when the Rectangle instance is deleted.
         """
         print("Bye rectangle...")
