3394f6f20a98a1b9852d7f3749c6be3075c46dac
agalotaibi <hopes.gmo@gmail.com>
2025-12-14 16:26:23 +0000

    solve task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the class MyList which inherits from list.
+"""
+
+
+class MyList(list):
+    """
+    A subclass of list that includes a method to print the list sorted.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending sorted order.
+        
+        Note:
+            This method prints a sorted copy of the list and does not
+            modify the actual instance of the list.
+        """
+        # We use the built-in sorted() function because it returns a new list
+        # and leaves the original list instance (self) unchanged.
+        print(sorted(self))
