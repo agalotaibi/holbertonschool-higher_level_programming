a24dc386958b7bdf46b5ee886c55852e2f961e89
agalotaibi <hopes.gmo@gmail.com>
2025-12-14 15:06:54 +0000

    solve task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module is all about returner list 
+of available attributes & methods of an object
+"""
 
 def lookup(obj):
     """return list of available attributes & methods of an object"""
