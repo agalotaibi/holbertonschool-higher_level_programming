3cd5685636672b11a7a9d69521beabc62e68c641
agalotaibi <hopes.gmo@gmail.com>
2025-12-15 14:10:11 +0000

    solve task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module have a method that check if the object is instance class
+or is an instance of a class that inherited from.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    method check if the object is exactly
+    an instance of class or class that inherited from.
+
+    Args:
+        obj: the object ot be check
+        a_class the class to check aganist
+
+    Retrun:
+        True: if the object is instance of class
+        or calss inherited from.
+        False: if it not instance of class
+        or calss inherited from.
+    """
+    return isinstance(obj, a_class)
