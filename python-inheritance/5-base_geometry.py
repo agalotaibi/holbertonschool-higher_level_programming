a00ebcc8f7ff61e3e79b5629b615f01324b97513
agalotaibi <hopes.gmo@gmail.com>
2025-12-15 14:16:35 +0000

    solve task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module is an empty class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    empty class BaseGeometry
+    """
+    class BaseGeometry:
+        pass
