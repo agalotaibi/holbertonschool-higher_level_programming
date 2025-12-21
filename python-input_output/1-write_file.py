67b82c64020fc24da9bbfaf873a72705e77fb91d
agaloatibi <hopes.gmo@gmail.com>
2025-12-21 14:35:23 +0000

    solve task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for write files"""
+
+
+def write_file(filename="", text=""):
+    """write a UTF-8 text file and 
+    returns the number of characters written.
+
+    Args:
+        filename: path to the file to open & write (default: "").
+        text: to be written in the filename.
+    
+    Return:
+        the number of charecters written
+    """
+
+    with open(filename,'w', encoding="utf-8") as f:
+        return f.write(text)
