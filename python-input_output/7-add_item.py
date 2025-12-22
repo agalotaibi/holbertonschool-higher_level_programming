49c631716d1cec5fc5eefeb84c9bba8d17219286
agalotaibi <hopes.gmo@gmail.com>
2025-12-22 07:27:31 +0000

    solve task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves to a JSON file"""
+"""Module that adds all arguments to a Python list and saves to a JSON file"""
 
 
 import sys
@@ -8,7 +8,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = ""
+filename = "add_item.json"
 
 try:
     items = load_from_json_file(filename)
