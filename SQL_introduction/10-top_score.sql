0d0f1c488611d4910f7fcc79196395e094010f77
agalotaibi <hopes.gmo@gmail.com>
2026-01-11 16:52:10 +0000

    solve task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- ists all records of the table
-0;276;0c-- Records should be ordered by score (top first)
+-- Records should be ordered by score (top first)
 
 SELECT score, name
 FROM second_table
