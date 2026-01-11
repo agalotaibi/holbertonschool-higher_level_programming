3966092eccfaf5c7d083abdd7b20600887cc8958
agalotaibi <hopes.gmo@gmail.com>
2026-01-11 16:44:34 +0000

    solve task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,18 @@
 -- creates a table second_table
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT
 );
+
+INSERT INTO second_table (id, name, score)
+VALUE (1, "John", 10);
+
+INSERT INTO second_table (ID, NAME, SCORE)
+VALUE (2, "Alex", 3);
+
+INSERT INTO second_table (ID, NAME, SCORE)
+VALUE (3, "Bob", 14)
+
+INSERT INTO second_table (ID, NAME, SCORE)
+VALUE (4, "George", 8)
