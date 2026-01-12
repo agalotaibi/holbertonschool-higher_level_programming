6172e7a558fb5e79a5a40074f3f9f37049d1fc73
agalotaibi <hopes.gmo@gmail.com>
2026-01-12 07:10:56 +0000

    solve task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- lists all records of the table without the null value
 SELECT score, name
 FROM second_table
-ORDER BY score DESC
-WHERE name IS NOT NULL;
+WHERE name IS NOT NULL
+ORDER BY score DESC;
+
