77925797d2c6ae60f98c0b850ac23e89cb6b83fd
agalotaibi <hopes.gmo@gmail.com>
2026-01-12 17:10:50 +0000

    solve task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
