26f5a053c1137464af138238ab726c65ac461a97
agalotaibi <hopes.gmo@gmail.com>
2026-01-12 07:02:28 +0000

    solve task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score
-11;rgb:2b2b/2b2b/2b2bSELECT score, COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER number DESC;
