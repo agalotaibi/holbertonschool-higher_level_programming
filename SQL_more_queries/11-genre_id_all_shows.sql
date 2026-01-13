a1e7431704d9c6519d6fd51ac7780465a17205a0
agalotaibi <hopes.gmo@gmail.com>
2026-01-13 16:49:38 +0000

    solve tasl 11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
 SELECT t.title, g.genre_id
 FROM tv_shows.title AS t
-LEFT JOIN tv_show_genres.genre_id AS g IS NOT NULL
+LEFT JOIN tv_show_genres.genre_id AS g
 ON t.id = g.show_id
 ORDER BY t.title, g.genre_id;
