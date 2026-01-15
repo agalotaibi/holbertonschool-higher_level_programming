be229b4ff66d165124e929157af15ebc68b2a994
agalotaibi <hopes.gmo@gmail.com>
2026-01-15 12:02:40 +0000

    solve task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 lists all states from the database hbtn_0e_0_usa
+# Usage: ./1-filter_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
 """
 
 
