ce72c90fd81110ccb7b469325c7edd32b2ce1dae
agalotaibi <hopes.gmo@gmail.com>
2026-01-16 11:07:53 +0000

    solve task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument from the database hbtn_0e_6_usa.
+Prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa.
 Safe from SQL injection.
 """
 import sys
