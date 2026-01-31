1efa2a01c011a4ae71f403e92aed0c6561973912
agalotaibi <hopes.gmo@gmail.com>
2026-01-31 09:30:55 +0000

    solve task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2).map(x => parseInt(x));
 if (args.length < 2) {
   console.log(0);
 } else {
-  let max = Math.max(...args);
+  const max = Math.max(...args);
   const filtered = args.filter(n => n !== max);
   console.log(Math.max(...filtered));
 }
