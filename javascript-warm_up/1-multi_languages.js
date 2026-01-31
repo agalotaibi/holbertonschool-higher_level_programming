08126a3608ed3a18cc38009e96dab61d893daa10
agalotaibi <hopes.gmo@gmail.com>
2026-01-31 08:55:07 +0000

    solve task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 
 const myVar0 = 'C is fun';
 const myVar1 = 'Python is cool';
-const myVar2 = 'Javascript is amazing';
+const myVar2 = 'JavaScript is amazing';
 console.log(myVar0);
 console.log(myVar1);
 console.log(myVar2);
